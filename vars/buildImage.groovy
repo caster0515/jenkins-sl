45d40f3dc71a4ddc071632da8d9dfbce596959ee
Ronald Sandoval <caster_marit@yahoo.com>
2025-02-24 16:08:07 +0100

    username

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+        sh 'docker build -t caster0515/demo-app:jma-2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push nanatwn/demo-app:jma-2.0'
     }
